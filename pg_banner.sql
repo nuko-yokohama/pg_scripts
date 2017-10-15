c22eb6edcfec4382bb2476d1d00d0abe5d325b54
nuko.yokohama <nuko.yokohama@gmail.com>
2017-10-16 07:52:11 +0900

    pg_banner.sql, bug-fix.

--- pg_banner.sql
+++ pg_banner.sql
@@ -540,7 +540,7 @@ SELECT 3 as id , '#  # # ' as tx  UNION
 SELECT 4 as id , '# #  # ' as tx  UNION
 SELECT 5 as id , '##   # ' as tx  UNION
 SELECT 6 as id , '#    # ' as tx  UNION
-SELECT 7 as id , ' ####   ' as tx  
+SELECT 7 as id , ' ####  ' as tx  
 ) AS t ORDER BY id
 $function$
 ;
